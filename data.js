8f930a5d8d44cfeec37e37152d27c81edd63b082
Alfiestuff <yepperyeppersone@gmail.com>
2026-01-21 16:59:09 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+var stations = [
+  { name: "Oxford Circus", lat: 51.515419, lng: -0.141099 },
+  { name: "King's Cross St. Pancras", lat: 51.530663, lng: -0.123194 },
+  { name: "Victoria", lat: 51.496424, lng: -0.144262 },
+  { name: "Bank", lat: 51.513314, lng: -0.089039 },
+  { name: "Liverpool Street", lat: 51.517372, lng: -0.083182 },
+  { name: "Waterloo", lat: 51.503299, lng: -0.113387 },
+  { name: "Paddington", lat: 51.515906, lng: -0.175579 },
+  { name: "Euston", lat: 51.528083, lng: -0.133816 },
+  { name: "London Bridge", lat: 51.505721, lng: -0.088557 },
+  { name: "Leicester Square", lat: 51.511289, lng: -0.128324 }
+];
